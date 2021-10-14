f77edf2cee62b00c6f20e22e46a2f7be593dd654
Himmel König <itogreniyorum@gmail.com>
2021-10-14 22:26:12 +0300

    forms changed

--- student/views.py
+++ student/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from student.models import Student
 from .forms import StudentForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
 
 def index(request):
     return render(request, 'student/index.html')
 
+
 def student_page(request):
-    	form = StudentForm()
-    	context = {
-        	'form': form
-    	}
-    	return render(request,'student/student.html', context)
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('student')
+    context = {
+        'form': form
+    }
+    return render(request, 'student/student.html', context)
